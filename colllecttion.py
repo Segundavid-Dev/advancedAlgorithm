c75e1faf23f3541288a02a937a35fe20e2f73c7e
Segun David <segdavid03@gmail.com>
2024-09-02 06:47:44 +0100

    commit again!!

--- colllecttion.py
+++ colllecttion.py
@@ -1,6 +1,9 @@
 # Collection modules in python implements special container data type and provides alterantives with some additional functionality instead of the general data type like dictionaries, list, sets, tuples
 from collections import Counter
 from collections import namedtuple
+from collections import OrderedDict
+from collections import defaultdict
+from collections import deque
 
 strings = 'aaaaabbbbccc'
 
@@ -19,4 +22,35 @@ print(pt)
 #orderdDictionaries
 #Same as the dictionary data type only that they remember the order in which items were inserted
 
+ordered_dict = OrderedDict()
+ordered_dict['a'] = 1
+ordered_dict['b'] = 2
+ordered_dict['c'] = 3
+ordered_dict['d'] = 4
+print(ordered_dict)
 
+#Defaultdict -- Returns the default value of the specified data type
+d = defaultdict(float)
+d['a'] = 1
+d['b'] = 2
+d['c'] = 3
+
+print(d['']) #Returns float default value
+
+
+#deque -- open ended queue data type, elememnts can be accessed from the head and tail
+dequeue = deque()
+dequeue.append('a')
+dequeue.append('b')
+dequeue.append('c')
+dequeue.appendleft('3') #append element from the left postion
+print(dequeue)
+
+dequeue.popleft() #remove elements from the left position
+print(dequeue)
+
+dequeue.extendleft([3,4,5])
+print(dequeue)
+
+dequeue.rotate(2)
+print(dequeue)
